6f6e673c3c399e497b3deb3b81b97960353fd12d
chepuri pavankumar <79352503+chepuripavankumar@users.noreply.github.com>
2022-02-26 19:53:28 +0530

    first loginpage

---
+++ src/components/Home.js
@@ -0,0 +1,77 @@
+import React,{Component, useState} from 'react';
+import "./styles.css";
+import "./util.css"; 
+
+import { Button } from 'react-bootstrap'; 
+export default function Home(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm(){
+    
+    console.log(email);
+    console.log(password);
+
+return true;
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+  }
+    
+        return(
+    
+            
+        <div className="limiter">
+          <div className="container-login100">
+            <div className="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+              <form className="login100-form validate-form flex-sb flex-w" onSubmit={handleSubmit}>
+                <span className="login100-form-title p-b-32">
+                  Account Login
+                </span>
+                <span className="txt1 p-b-11">
+                  Username
+                </span>
+                <div className="wrap-input100 validate-input m-b-36" data-validate="Username is required">
+                  <input className="input100" type="text" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+                  <span className="focus-input100" />
+                </div>
+                <span className="txt1 p-b-11">
+                  Password
+                </span>
+                <div className="wrap-input100 validate-input m-b-12" data-validate="Password is required">
+                  <span className="btn-show-pass">
+                    <i className="fa fa-eye" />
+                  </span>
+                  <input className="input100" type="password" name="password"value={password} onChange={(e)=>setPassword(e.target.value)} />
+                  <span className="focus-input100"/>
+                </div>
+                <div className="flex-sb-m w-full p-b-48">
+                  <div className="contact100-form-checkbox">
+                    <input className="input-checkbox100" id="ckb1" type="checkbox" name="remember-me" />
+                    <label className="label-checkbox100" htmlFor="ckb1">
+                      Remember me
+                    </label>
+                  </div>
+                  <div>
+                    <a href="#" className="txt3">
+                      Forgot Password?
+                    </a>
+                  </div>
+                </div>
+                <div className="container-login100-form-btn">
+                  <button type='button' className='btn btn-primary' onClick={()=>{validateForm()}}>
+                    Login
+                  </button>
+                </div>
+              </form>
+        <div id="dropDownSelect1" />
+      </div>
+      </div>
+      </div>
+ 
+    
+        );
+        }
+       
+       
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import  "./components/styles.css";  
+import "./components/util.css";
+import App from "./App";
+import Home from "./components/Home";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+import { Button } from 'react-bootstrap'; 
+
+ReactDOM.render (
+  
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
